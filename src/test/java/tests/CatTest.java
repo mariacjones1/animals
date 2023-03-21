56c1f49c446987fb880969910088e5eb0965311e
Maria <mariacjones1@hotmail.co.uk>
2023-03-21 16:08:32 +0000

    Comment out sleep test

--- src/test/java/tests/CatTest.java
+++ src/test/java/tests/CatTest.java
@@ -20,11 +20,11 @@ public class CatTest {
         assertEquals("chicken", Cat.lastAte, "Cat last ate does not match.");
     }
 
-    @Test
-    public void catSleepTest() {
-        Cat testCat = new Cat();
-        assertFalse(testCat.sleep());
-    }
+//    @Test
+//    public void catSleepTest() {
+//        Cat testCat = new Cat();
+//        assertFalse(testCat.sleep());
+//    }
 
 
 }
